8c41db0ef7f1574d0f8ad3358790dca6ff2918bc
Alan Langford <instance1@gmail.com>
2023-07-02 19:10:14 -0400

    Report core and API versions.

---
+++ src/Helpers/Version.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Abivia\Ledger\Helpers;
+
+class Version
+{
+    public static function api(): string
+    {
+        return '1.1.0';
+    }
+
+    public static function core(): string
+    {
+        return '1.11.0';
+    }
+
+}
